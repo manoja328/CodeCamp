a990884970b26ddb5572a907467e49a974b16e1e
Pravesh Koirala <praveshkoirala@gmail.com>
2013-06-29 08:59:04 +0545

    Map shown

--- CodeCamp/CodeCamp/urls.py
+++ CodeCamp/CodeCamp/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import patterns, include, url
 
+from DisasterViz import views
+
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
@@ -8,7 +10,8 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'CodeCamp.views.home', name='home'),
     # url(r'^CodeCamp/', include('CodeCamp.foo.urls')),
-
+    url(r'^$',views.index),
+    url(r'^Nepal_Zones.svg',views.image),
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
--- CodeCamp/DisasterViz/views.py
+++ CodeCamp/DisasterViz/views.py
@@ -1,1 +1,11 @@
 # Create your views here.
+from django.http import *
+
+def index(request):
+    return HttpResponse('<img src="Nepal_Zones.svg"></img>')
+
+def image(request):
+    a = HttpResponse(open('Nepal_Zones.svg','r').read(),mimetype="image/svg+xml")
+    return a
+
+
